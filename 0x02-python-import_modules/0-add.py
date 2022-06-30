7e1e949bab6016e7aff9f4a97c374b0cd3931606
Amha-Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2022-06-30 13:57:42 +0300

    added answer to question-0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Imports add_0 module.
+Prints the sum of two nummbers using this module.
+Uses format string to display results.
+
+"""
+from add_0 import add
+
+a = 1
+b = 2
+
+print ("{} + {} = {}".format(a,b, add(a,b))) 
