49deed968c9ba0e98309d05c2a3f6cc5f7626077
Amha-Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2022-06-30 12:46:26 +0000

    minor fixes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-import sys
-
-argLength = len(sys.argv)
-
-if argLength == 1:
-    print("0 arguments.")
-else:    
-    if argLength == 2:
-        print((argLength - 1), "argument:")
-    else:
-        print((argLength - 1), "arguments:")      
-    for i in range(1, len(sys.argv)):
-        print("{:d}:".format(i) , sys.argv[i])
+if __name__ == "__main__":
+    import sys
+    argLength = len(sys.argv)
+    if argLength == 1:
+        print("0 arguments.")
+    else:    
+        if argLength == 2:
+            print((argLength - 1), "argument:")
+        else:
+            print((argLength - 1), "arguments:")      
+        for i in range(1, len(sys.argv)):
+            print("{:d}:".format(i) , sys.argv[i])
