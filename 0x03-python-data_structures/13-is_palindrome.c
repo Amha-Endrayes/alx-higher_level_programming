d99b0be6346d55aa187221a40de3263bffc454c9
Amha Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2022-07-06 02:59:16 +0300

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,23 +1,72 @@
 #include "lists.h"
 
+listint_t *reverse_list(listint_t **head);
+int is_palindrome(listint_t **head);
+
 /**
- * is_palindrome - checks if palindrome
- * @head: head of node
- * Return: 0 if not, 1 if is
+ * reverse_list - Reverses a singly-linked listint_t list.
+ * @head: Points to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_list(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: Points to the head of the linked list.
+ *
+ * Return: 0 - If not a palindrome.
+ *         1 - If the is a palindrome.
  */
 int is_palindrome(listint_t **head)
 {
-	unsigned int len = 1;
-	listint_t *temp;
+	listint_t *tmpo, *reverse, *middle;
+	size_t size = 0, i;
 
-	if (head == NULL|| *head == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	temp = *head;
-	while (temp) /* get len of list */
+	tmpo = *head;
+	while (tmpo)
 	{
-		temp = temp->next;
-		len++;
+		size++;
+		tmpo = tmpo->next;
 	}
-	return (0);
+
+	tmpo = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmpo = tmpo->next;
+
+	if ((size % 2) == 0 && tmpo->n != tmpo->next->n)
+		return (0);
+
+	tmpo = tmpo->next->next;
+	reverse = reverse_list(&tmp);
+	middle = reverse;
+
+	tmpo = *head;
+	while (reverse)
+	{
+		if (tmpo->n != reverse->n)
+			return (0);
+		tmpo = tmpo->next;
+		reverse = reverse->next;
+	}
+	reverse_list(&middle);
+
+	return (1);
 }
