87f8edf5bff249038266f18a38c5dc228cbb0757
Amha-Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2022-10-04 02:30:36 +0300

    made spelling corrections

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const { argv } = require('node:process');
 
 if (argv.length < 3) {
-  console.log('No Arguments');
+  console.log('No Argument');
 } else if (argv.length === 3) {
   console.log('Argument found');
 } else {
