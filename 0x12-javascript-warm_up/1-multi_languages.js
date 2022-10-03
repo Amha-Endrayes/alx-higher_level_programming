4080814bcc631a852638e3c7b7d3427ed63de017
Amha-Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2022-10-04 02:36:40 +0300

    added minor correction

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const multiMsg = 'C is fun\n' + 'Python is cool\n' + 'JavaScript is amazing\n';
+const multiMsg = 'C is fun\n' + 'Python is cool\n' + 'JavaScript is amazing';
 console.log(multiMsg);
