7a20b4a09ef7f0f9fe1e31e41cb0d277b484a6fa
Amha-Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2022-10-04 02:41:22 +0300

    added minor correction

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-if (argv.length < 3) {
+if (argv.length === 2) {
   console.log('No Argument');
 } else if (argv.length === 3) {
   console.log('Argument found');
