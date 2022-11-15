8c9e8528c4f591040da5c03490c71b9031cfb61a
Amha-Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2022-11-16 00:57:36 +0300

    minor fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,14 @@
 #!/usr/bin/node
-// JS Script
-const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', (error, data) => {
-  console.log(error || data);
+// JS Script to read a file
+const process = require('process');
+const filesystem = require('fs');
+
+let file = process.argv[2];
+
+filesystem.readFile(file, 'utf8', function (err, data) {
+  if (err != null) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
 });
