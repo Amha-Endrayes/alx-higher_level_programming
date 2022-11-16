f5078a1fbeb42d7564c82a8f0aab5fe6b36e2251
Amha-Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2022-11-16 05:01:47 +0300

    added advanced tasks

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,43 @@
+#!/usr/bin/node
+const process = require('process');
+const request = require('request');
+const order = [];
+const responses = {};
+
+function getCharName (charUrl) {
+  let val;
+  val = request(charUrl, function (error, response, body) {
+    if (error != null) {
+      console.log(error);
+    } else {
+      const data = JSON.parse(body);
+      val = data.name;
+      responses[charUrl] = val;
+    }
+  });
+}
+
+function doParse () {
+  const movie = process.argv[2];
+  const url = 'https://swapi-api.hbtn.io/api/films/' + movie;
+
+  request(url, function (error, response, body) {
+    if (error != null) {
+      console.log(error);
+    } else {
+      const data = JSON.parse(body);
+      data.characters.forEach(function (charUrl) {
+        order.push(charUrl);
+        getCharName(charUrl);
+      });
+    }
+  });
+}
+
+doParse();
+setTimeout(function () {
+  // some stuff
+  order.forEach(function (url) {
+    console.log(responses[url]);
+  });
+}, 5000);
